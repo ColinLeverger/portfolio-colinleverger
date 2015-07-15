bcf5abdb4949b317bf1e219be093ef5fd433ab25
Colin LEVERGER <colinleverger@gmail.com>
2015-07-15 12:29:04 +0200

    link header

--- portfolio/header.php
+++ portfolio/header.php
@@ -7,7 +7,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php">STANLEY</a>
+            <a class="navbar-brand" href="index.php">Colin</a>
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
