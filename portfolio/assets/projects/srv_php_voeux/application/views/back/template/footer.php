5d3cf71f9ed1110362c8dacde50e16f2019d3113
Colin LEVERGER <colinleverger@gmail.com>
2016-01-19 19:38:23 +0100

    Refactor of source location

--- portfolio/assets/projects/srv_php_voeux/application/views/back/template/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: zahead
- * Date: 26/05/15
- * Time: 12:57
- */
-?>
-</div>
--- portfolio/assets/projects/srv_php_voeux/application/views/js/jsAdminLoad.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Permet de charger le js spécifique au panel admin
- * Created by PhpStorm.
- * User: zahead
- * Date: 29/05/15
- * Time: 22:13
- */?>
-<script src="<?php echo base_url();?>assets/js/admin.js"></script>
