49616dc2e2274812c5126049e4e35cbdb050bb1e
Colin LEVERGER <colinleverger@gmail.com>
2016-01-19 19:39:09 +0100

    Refactor: added new files

---
+++ header.php
@@ -0,0 +1,21 @@
+<!-- Static navbar -->
+<div id="navWrap">
+    <div class="navbar navbar-inverse  navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#top">Colin LEVERGER</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#about">About me</a></li>
+                    <li><a href="#skills">Skills</a></li>
+                    <li><a href="#devops">DevOps</a></li>
+                    <li><a href="#work">Work</a></li>
+                    <li><a href="#footer">Contact</a></li>
+                    <li><a href="assets/pdf/Colin-LEVERGER.pdf">CV</a></li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </div>
+</div>
