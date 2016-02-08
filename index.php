7cd1e058b56c522a58c423065631342ea06e8e50
Colin LEVERGER <colinleverger@gmail.com>
2016-02-08 23:36:21 +0100

    Add Devops Keyword

--- index.php
+++ index.php
@@ -231,7 +231,7 @@ include "header.php";
     <div class="container pt" id="devops">
         <div class="row ">
             <div class="col-lg-6 col-lg-offset-3 centered">
-                <h3>My typical workflow</h3>
+                <h3>My typical DevOps workflow</h3>
                 <hr>
             </div>
         </div>
